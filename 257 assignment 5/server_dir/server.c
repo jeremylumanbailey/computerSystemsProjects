86d36cd09b6fc9f0f0d3e7c3441fd1a5f11f1f37
Jeremy Latitude E6410 <jeremylumanbailey@gmail.com>
2019-04-19 15:55:05 -0400

    backin' up assignment

---
+++ 257 assignment 5/server_dir/server.c
@@ -0,0 +1,147 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+ // #define PORT "8570"  // the port users will be connecting to
+
+
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[]) {
+	char *D;
+
+	int PORT = strtol(argv[1], &D, 10);		
+
+	//will return -1 if cant open 
+	int fd = open(argv[3], O_RDWR);
+
+/*
+
+	if(PORT < 50000 || PORT > 60000) {
+		fprintf(stderr,"Port out of range \n");
+	    exit(1);
+	}*/
+
+	// printf(%d\, PORT);
+
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	int yes=1;
+
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+		if (argc != 2) {
+	    fprintf(stderr,"Need port argument\n");
+	    exit(1);
+	}
+
+
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+    int PID;
+
+	while(1) {  // main accept() loop
+	
+       
+        sin_size = sizeof their_addr;
+		
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}//	wait(NULL);
+        PID = fork();
+
+
+        //if statement is child function, else is parent function
+        if(PID == 0) {
+		    inet_ntop(their_addr.ss_family,
+			    get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+		    printf("server: got connection from %s\n", s);
+
+		    if (send(new_fd, "Hello, world!", 13, 0) == -1)
+				perror("send");
+            close(new_fd);
+            exit(0);
+        }
+        else { 
+            close(new_fd);
+        }
+	}
+
+	return 0;
+}
